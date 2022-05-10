ef7bc719e66d3eaf4c6ad1ab8d5ec644ba2efedc
HarbingerZou <90372397+HarbingerZou@users.noreply.github.com>
2022-05-10 16:02:36 +0800

    Add files via upload

--- temp/temp/model/helpers.swift
+++ temp/temp/model/helpers.swift
@@ -7,12 +7,37 @@
 
 import Foundation
 
+//get the hours and minutes part of a date 
 func get_string_from_date(date:Date) ->String{
     let formatter = DateFormatter()
     formatter.dateFormat = "HH:mm"
     return formatter.string(from: date)
 }
 
+//validate if a email is in the correct format
 func vaild_email(_ email:String) -> Bool{
     return true
+}
+
+//json loader
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
 }
